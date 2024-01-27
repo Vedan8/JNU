316cb8fd5173b69c418b38e76474709e1db2d450
Vedan8 <vedanjyadav@gmail.com>
2024-01-27 18:09:38 +0530

    resetPassward

--- account/serializers.py
+++ account/serializers.py
@@ -81,7 +81,7 @@ class SendPasswordResetEmailSerializer(serializers.Serializer):
       print('Encoded UID', uid)
       token = PasswordResetTokenGenerator().make_token(user)
       print('Password Reset Token', token)
-      link = 'https://auth-api-jexl.onrender.com/'+uid+'/'+token
+      link = 'https://auth-api-jexl.onrender.com/reset-password/+'+uid+'/'+token
       print('Password Reset Link', link)
       # Send EMail
       body = 'Click Following Link to Reset Your Password '+link
--- account/views.py
+++ account/views.py
@@ -57,30 +57,39 @@ class UserChangePasswordView(APIView):
     serializer.is_valid(raise_exception=True)
     return Response({'msg':'Password Changed Successfully'}, status=status.HTTP_200_OK)
 
-class SendPasswordResetEmailView(APIView):
-  renderer_classes = [UserRenderer]
-  def post(self, request, format=None):
-    serializer = SendPasswordResetEmailSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    my_recipient=serializer.data['email']
-    otp_code = str(random.randint(100000, 999999))
-
-        # Save the OTP to the database
-    send_mail(
-            'Password Reset',
-            f'Reset your password using the following link {otp_code}',
-            'otpkjaef@gmail.com',
-            [my_recipient],
-            fail_silently=False,
-        )
-    return Response({'msg':'Password Reset link send. Please check your Email'}, status=status.HTTP_200_OK)
-
 class UserPasswordResetView(APIView):
   renderer_classes = [UserRenderer]
   def post(self, request, uid, token, format=None):
     serializer = UserPasswordResetSerializer(data=request.data, context={'uid':uid, 'token':token})
     serializer.is_valid(raise_exception=True)
     return Response({'msg':'Password Reset Successfully'}, status=status.HTTP_200_OK)
+
+
+
+class SendPasswordResetEmailView(APIView):
+  renderer_classes = [UserRenderer]
+  def post(self, request, format=None):
+    serializer = SendPasswordResetEmailSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    # my_recipient=serializer.data['email']
+    # otp_code = str(random.randint(100000, 999999))
+
+        # Save the OTP to the database
+    # send_mail(
+    #         'Password Reset',
+    #         f'Reset your password using the following link {otp_code}',
+    #         'otpkjaef@gmail.com',
+    #         [my_recipient],
+    #         fail_silently=False,
+    #     )
+    return Response({'msg':'Password Reset link send. Please check your Email'}, status=status.HTTP_200_OK)
+
+# class UserPasswordResetView(APIView):
+#   renderer_classes = [UserRenderer]
+#   def post(self, request, format=None):
+#     serializer = UserChangePasswordSerializer(data=request.data, context={'user':request.user})
+#     serializer.is_valid(raise_exception=True)
+#     return Response({'msg':'Password Changed Successfully'}, status=status.HTTP_200_OK)
 
 # from django.views.decorators.csrf import csrf_exempt
 
